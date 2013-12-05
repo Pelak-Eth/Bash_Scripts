04e5658b52aff3d4d9d568c855b81f7c587884c6
joshpelak <joshpelak@gmail.com>
2013-12-05 18:31:52 -0500

    Create runfirst.sh
    Run this script after downloading all scripts to the same location.
    This script will symlink all other scripts to /usr/local/bin so they can be called by name from anywhere.

---
+++ runfirst.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo "You are located in $DIR"
+echo "Do you want to create Symlinks to /usr/local/bin for all scripts?
+This will allow you to call the script using just the name from anywhere"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes )
+echo "Creating Symlinks Now.....";
+# Array of scripts to be symlinked to /usr/local/bin
+script_names=(
+
+orm
+server-install
+ssh-setup
+
+)
+#symlink all names listed in script_names array
+for scriptname in "${script_names[@]}"
+do
+	sudo ln -s $DIR/$scriptname.sh /usr/local/bin/$scriptname
+done
+echo "Symlinking Complete
+You may now call each script from anywhere!
+Example type: orm [ENTER]"
+break;;
+        No ) exit;;
+    esac
+done
