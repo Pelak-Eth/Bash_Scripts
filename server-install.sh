d6a1737990f898384250335046707627661a0734
joshpelak <joshpelak@gmail.com>
2013-12-10 00:09:32 -0500

    Update server-install.sh

--- server-install.sh
+++ server-install.sh
@@ -70,7 +70,7 @@ select yn in "Yes" "No"; do
     case $yn in
         Yes )
 echo "Installing git now" 
-sudo apt-get install git
+sudo apt-get install git -y
 echo "Setting up Git config";
 # Set git user info
 echo -n "Enter git username and press [ENTER]: ";
@@ -166,12 +166,12 @@ select yn in "Yes" "No"; do
     case $yn in
         Yes )
 #install required packages first
-sudo apt-get install php-pear curl php5 php5-cli
+sudo apt-get install php-pear curl php5 php5-json php5-cli -y
 sudo pear config-set auto_discover 1
 #Global Composer install
 echo "Installing Composer Globally";
-curl -sS https://getcomposer.org/installer | php;
-sudo mv composer.phar /usr/local/bin/composer;
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
 #PHP-UNIT
 echo "Installing PHP-UNIT Testing";
 sudo pear channel-discover pear.symfony.com;
@@ -245,122 +245,4 @@ break;;
         No ) break;;
     esac
 done;
-tput setaf 7
-hacked() {
-cat <<"EOT"
-
- _   _   ____  _  ___  _______     _   _  __   __  _ _______________ 
-| | | | / __ \| |/ / |/ /___ /  __| | | |_\ \ / / / |___ /___ /___  |
-| |_| |/ / _` | ' /| ' /  |_ \ / _` | | '_ \ V /  | | |_ \ |_ \  / / 
-|  _  | | (_| | . \| . \ ___) | (_| | | |_) | |   | |___) |__) |/ /  
-|_| |_|\ \__,_|_|\_\_|\_\____/ \__,_| |_.__/|_|   |_|____/____//_/   
-        \____/                                                       
- _   _   ____  _  ___  _______        ____  
-| | | | / __ \| |/ / |/ /___ / _ __  |  _ \ 
-| |_| |/ / _` | ' /| ' /  |_ \| '__| | |_) |
-|  _  | | (_| | . \| . \ ___) | |    |  __/ 
-|_| |_|\ \__,_|_|\_\_|\_\____/|_|    |_|    
-        \____/                              
-
-
-EOT
-}
-tput setaf 1
-countdown() {
-cat <<"EOT"
-  ____                            _             __        ___ _ _ 
- / ___|___  _ __ ___  _ __  _   _| |_ ___ _ __  \ \      / (_) | |
-| |   / _ \| '_ ` _ \| '_ \| | | | __/ _ \ '__|  \ \ /\ / /| | | |
-| |__| (_) | | | | | | |_) | |_| | ||  __/ |      \ V  V / | | | |
- \____\___/|_| |_| |_| .__/ \__,_|\__\___|_|       \_/\_/  |_|_|_|
-                     |_|                                          
- ____       _  __   ____            _                   _     _         _____ 
-/ ___|  ___| |/ _| |  _ \  ___  ___| |_ _ __ _   _  ___| |_  (_)_ __   |___ / 
-\___ \ / _ \ | |_  | | | |/ _ \/ __| __| '__| | | |/ __| __| | | '_ \    |_ \ 
- ___) |  __/ |  _| | |_| |  __/\__ \ |_| |  | |_| | (__| |_  | | | | |  ___) |
-|____/ \___|_|_|   |____/ \___||___/\__|_|   \__,_|\___|\__| |_|_| |_| |____/ 
-                                                                              
-                              _       
- ___  ___  ___ ___  _ __   __| |___ _ 
-/ __|/ _ \/ __/ _ \| '_ \ / _` / __(_)
-\__ \  __/ (_| (_) | | | | (_| \__ \_ 
-|___/\___|\___\___/|_| |_|\__,_|___(_)
-                                     
-EOT
-}
-asc3() {
-cat <<"EOT"
- _____ 
-|___ / 
-  |_ \ 
- ___) |
-|____/ 
-      
-EOT
-}
-asc2() {
-cat <<"EOT"
- ____  
-|___ \ 
-  __) |
- / __/ 
-|_____|
-     
-EOT
-}
-asc1() {
-cat <<"EOT"
- _ 
-/ |
-| |
-| |
-|_|
-   
-EOT
-}
-explosion() {
-cat <<"EOT"
-                             ____                              
-               ____  , -- -        ---   -.                    
-            (((   ((  ///   //   '  \\-\ \  )) ))              
-        ///    ///  (( _        _   -- \\--     \\\ \)         
-     ((( ==  ((  -- ((             ))  )- ) __   ))  )))       
-      ((  (( -=   ((  ---  (          _ ) ---  ))   ))         
-         (( __ ((    ()(((  \\  / ///     )) __ )))            
-                \\_ (( __  |     | __  ) _ ))                  
-                          ,|  |  |                             
-                         `-._____,-'                           
-                         `--.___,--'                           
-                           |     |                             
-                           |    ||                             
-                           | ||  |                             
-                 ,    _,   |   | |                             
-        (  ((  ((((  /,| __|     |  ))))  )))  )  ))           
-      (()))       __/ ||(    ,,     ((//\     )     ))))       
----((( ///_.___ _/    ||,,_____,_,,, (|\ \___.....__..  ))--ool
-           ____/      |/______________| \/_/\__                
-          /                                \/_/|               
-         /  |___|___|__                        ||     ___      
-         \    |___|___|_                       |/\   /__/|     
-         /      |   |                           \/   |__|/     
-                                                               
-EOT
-}
-hacked;
-sleep 3;
-countdown;
-sleep 1;
-echo -e "\n\n\n\n"
-asc3;
-sleep 1;
-echo -e "\n\n\n\n"
-asc2;
-sleep 1;
-echo -e "\n\n\n\n"
-asc1;
-sleep 1;
-echo -e "\n\n\n\n"
-explosion;
-sleep 5;
-exit;
 exit;
